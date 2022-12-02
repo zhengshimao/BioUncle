28d0cf8073a88a74c8afcf1d32f2cc33f6c08a74
zhengshimao <71577617+zhengshimao@users.noreply.github.com>
2022-12-02 11:37:08 +0800

    modified

--- R/enricherGO.R
+++ R/enricherGO.R
@@ -5,7 +5,7 @@
 #' @title enricherGO
 #' @param gene See \link[clusterProfiler]{enricher}.
 #' @param go2ont a data frame with "go_id’ and "Ontology". If `NULL`, it will be downloaded every time. It is recommended to save the `go2ont` with the `get_GO2Ontology_table()` function first.
-#' @param ont GO Ontology: "BP", "MF", "CC", "ALL".
+#' @param ont GO Ontology: "BP", "MF", "CC", "ALL". See \link[clusterProfiler]{enrichGO}.
 #' @param pvalueCutoff See \link[clusterProfiler]{enricher}.
 #' @param qvalueCutoff See \link[clusterProfiler]{enricher}.
 #' @param pAdjustMethod See \link[clusterProfiler]{enricher}.
