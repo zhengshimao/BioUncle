1390759a511b400ce79c03e5e1c3e03c4b997a58
zhengshimao <71577617+zhengshimao@users.noreply.github.com>
2022-12-04 16:21:30 +0800

    Update get_one2nul.R

--- R/get_one2mul.R
+++ R/get_one2mul.R
@@ -4,30 +4,35 @@
 #'
 #' @param df A data frame with two columns.
 #' @param sep A separator character.
+#' @param rename A vector of new column names of length 2.
 #'
 #' @return A data frame with two character vectors.
 #' @export
 #'
 #' @examples
 #' \dontrun{
+#' library(dplyr)
+#'
 #' df <- iris %>% select(5,3)
 #' str(df)
-#' new_df <- get_one2mul(df)
 #'
+#' new_df <- get_one2mul(df)
 #' str(new_df)
 #' }
 get_one2mul <- function(df,
-                        sep = "/"
+                        sep = "/",
+                        rename = NULL
                         ){
   stopifnot(is.data.frame(df)) # df
-
-  stopifnot(ncol(df) == 2) #能力暂时只能做2列数据的，凑合用吧
+  stopifnot(ncol(df) == 2) # df: 2 columns
+  if(!is.null(rename)){stopifnot(length(rename) == 2)}
 
   df_name <- names(df)
   names(df) <- c("A","B")
-
   df$A <- df$A %>% as.character()
-  res <- data.frame(A = character(0), B = character(0)) #结果每列会变为字符串向量。可以根据输入df判断写活。
+  df$B <- df$B %>% as.character()
+  # a new data frame with two columns
+  res <- data.frame(A = character(0), B = character(0)) #two character vecttors.
 
   for (i in 1:nrow(df)) {
 
@@ -35,14 +40,18 @@ get_one2mul <- function(df,
       position_nrow <- which(res$A %in% df[i,1])
       res[position_nrow,2] <- paste(res[position_nrow,2],df[i,2], sep = "/")
     }else{
-      # gene 首次出现
+      # a new content
       res[nrow(res)+1,1] <- df[i,1]
-      res[nrow(res),2] <- df[i,2] #可不要再给行数＋1了
+      res[nrow(res),2] <- df[i,2] #don't add the number of row again.
     }
 
   }
+  if(is.null(rename)){
+    names(res) <- df_name
+  }else{
+    names(res) <- rename
+  }
 
-  names(res) <- df_name
   return(res)
 
 }
